b21fa9a974a21faf840dd8b803e7c4dfb61e4b4c
xxAROX <arox.email.0@gmail.com>
2022-07-29 03:16:26 +0200

    Fixed: If no waterdog data were provided and kick is disabled.

--- src/WDFix.php
+++ src/WDFix.php
@@ -126,47 +126,52 @@ class WDFix extends PluginBase implements Listener{
 				return;
 			}
 			$event->getOrigin()->setHandler(
-				new class(Server::getInstance(), $event->getOrigin(), function (XboxLivePlayerInfo $info) use ($event, $clientData, $packet): void{
-				$class = new ReflectionClass($event->getOrigin());
-				$property = $class->getProperty("info");
-				$property->setAccessible(true);
-				$property->setValue($event->getOrigin(), new XboxLivePlayerInfo($clientData["Waterdog_XUID"], $info->getUsername(), $info->getUuid(), $info->getSkin(), $info->getLocale(), $info->getExtraData()));
-			}, function (bool $isAuthenticated, bool $authRequired, ?string $error, ?string $clientPubKey) use ($event): void{
-				$class = new ReflectionClass($event->getOrigin());
-				$method = $class->getMethod("setAuthenticationStatus");
-				$method->setAccessible(true);
-				$method->invoke($event->getOrigin(), $isAuthenticated, $authRequired, $error, $clientPubKey);
-			}) extends LoginPacketHandler{
-				/**
-				 * Function parseClientData
-				 * @param string $clientDataJwt
-				 * @return ClientData
-				 */
-				protected function parseClientData(string $clientDataJwt): ClientData{
-					try {
-						[, $clientDataClaims,] = JwtUtils::parse($clientDataJwt);
-					} catch (JwtException $e) {
-						throw PacketHandlingException::wrap($e);
-					}
-					$mapper = new \JsonMapper;
-					$mapper->bEnforceMapType = false;
-					$mapper->bExceptionOnMissingData = true;
-					$mapper->bExceptionOnUndefinedProperty = true;
-					try {
-						$properties = array_map(fn(\ReflectionProperty $property) => $property->getName(), (new ReflectionClass(ClientData::class))->getProperties());
-						foreach ($clientDataClaims as $k => $v) {
-							if (!in_array($k, $properties)) {
-								unset($clientDataClaims[$k]);
-							}
+				new class(
+					Server::getInstance(),
+					$event->getOrigin(), function (XboxLivePlayerInfo $info) use ($event, $clientData, $packet): void{
+						if (isset($clientData["Waterdog_XUID"])) {
+							$class = new ReflectionClass($event->getOrigin());
+							$property = $class->getProperty("info");
+							$property->setAccessible(true);
+							$property->setValue($event->getOrigin(), new XboxLivePlayerInfo($clientData["Waterdog_XUID"], $info->getUsername(), $info->getUuid(), $info->getSkin(), $info->getLocale(), $info->getExtraData()));
 						}
-						unset($properties);
-						$clientData = $mapper->map($clientDataClaims, new ClientData);
-					} catch (JsonMapper_Exception $e) {
-						throw PacketHandlingException::wrap($e);
+					},
+					function (bool $isAuthenticated, bool $authRequired, ?string $error, ?string $clientPubKey) use ($event): void{
+						$class = new ReflectionClass($event->getOrigin());
+						$method = $class->getMethod("setAuthenticationStatus");
+						$method->setAccessible(true);
+						$method->invoke($event->getOrigin(), $isAuthenticated, $authRequired, $error, $clientPubKey);
+					}) extends LoginPacketHandler{
+						/**
+						 * Function parseClientData
+						 * @param string $clientDataJwt
+						 * @return ClientData
+						 */
+						protected function parseClientData(string $clientDataJwt): ClientData{
+							try {
+								[, $clientDataClaims,] = JwtUtils::parse($clientDataJwt);
+							} catch (JwtException $e) {
+								throw PacketHandlingException::wrap($e);
+							}
+							$mapper = new \JsonMapper;
+							$mapper->bEnforceMapType = false;
+							$mapper->bExceptionOnMissingData = true;
+							$mapper->bExceptionOnUndefinedProperty = true;
+							try {
+								$properties = array_map(fn(\ReflectionProperty $property) => $property->getName(), (new ReflectionClass(ClientData::class))->getProperties());
+								foreach ($clientDataClaims as $k => $v) {
+									if (!in_array($k, $properties)) {
+										unset($clientDataClaims[$k]);
+									}
+								}
+								unset($properties);
+								$clientData = $mapper->map($clientDataClaims, new ClientData);
+							} catch (JsonMapper_Exception $e) {
+								throw PacketHandlingException::wrap($e);
+							}
+							return $clientData;
+						}
 					}
-					return $clientData;
-				}
-			}
 			);
 			if (isset($clientData["Waterdog_IP"])) {
 				$class = new ReflectionClass($event->getOrigin());
